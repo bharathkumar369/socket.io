6df804a5ccff2278e7c2c79fd918e4c98cc04846
bathu <bharathpathapati@gmail.com>
2024-04-25 10:59:41 +0530

    complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,36 +4,72 @@ import Input from "./component/Input";
 import "./App.css";
 
 function App() {
+  const [score, setScore] = React.useState({});
+  const [allScores,setAllScores] = React.useState([]);
 
-  const [score,setScore] = React.useState({})
-
-  const socket = io("localhost:3000");
+  const socket = io("localhost:3001");
 
   const connectSocket = () => {
-    socket.on(
-      "connection",
-      (socket) => {
-        console.log(socket);
-      },
-      []
-    );
+    socket.on("connection",(socket) => {
+      console.log(socket);
+    });
   };
   React.useEffect(() => {
     connectSocket();
-  });
+  },[]);
 
   const handleInput = (event) => {
-    let {name,value} = event.target
+    let { name, value } = event.target;
     // console.log({[name]:value});
-    let currentObject = {[name]:value};
+    let currentObject = { [name]: value };
 
-    setScore((prev)=>({...prev,...currentObject}))
-  }
+    setScore((prev) => ({ ...prev, ...currentObject }));
+  };
+
+  const sendScores = () => {
+    console.log(score);
+
+    socket.emit("scores",score);
+
+    socket.on("playerScore",(playerDetails)=>{
+      setAllScores(playerDetails);
+    })
+  };
+
   return (
     <main>
       <h1>REACT MULTIPLAyER </h1>
-      <Input placeholder="enter your name" name="name" handleInput={handleInput}/>
-      <Input placeholder="enter your score" name="score" handleInput={handleInput}/>
+      <Input
+        placeholder="enter your name"
+        name="name"
+        handleInput={handleInput}
+      />
+      <Input
+        placeholder="enter your score"
+        name="score"
+        handleInput={handleInput}
+      />
+
+      <button onClick={sendScores}>Publish score</button>
+
+      {allScores.length>0 ? <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+
+          </tr>
+
+          {allScores.map((score)=>(
+            <tr>
+              <td>{score?.name}</td>
+              <td>{score?.score}</td>
+            </tr>
+          ))}
+        </tbody>
+        
+      </table> : <></>}
+
     </main>
   );
 }
--- server/server.js
+++ server/server.js
@@ -3,17 +3,29 @@ const {Server} = require("socket.io");
 
 const httpServer = createServer();
 
-const socket = new Server(httpServer,{
+const io = new Server(httpServer,{
     cors:{
         origin: "http://localhost:5173",
     }
 });
 
-socket.on("connection", (socket)=>{
-    console.log(socket);
+let playerDetails = []
+
+io.on("connection", (socket)=>{
+    socket.on("scores",(score)=>{
+        playerDetails.push({...score,id:socket.id});
+
+        console.log(playerDetails);
+    
+        socket.emit("playerScore",playerDetails);
+
+        setInterval(()=>{
+            socket.emit("playerScore",playerDetails)
+        },5000);
+    })
 
 });
 
-httpServer.listen(3000, () => {
+httpServer.listen(3001, () => {
     console.log("server is connected!");
 })
